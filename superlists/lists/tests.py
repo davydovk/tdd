afa2ce2da3be7d3b278b676c5c257d0d52027304
davydovk <davydovk1986@gmail.com>
2023-01-25 12:40:23 +0300

    Добавлены добавления элементов в таблицу и их проверки

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,5 +8,12 @@ from .views import home_page
 class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
+        """ Тест: используется домашний шаблон """
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """ Тест: можно сохранить POST-запрос """
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
